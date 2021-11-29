3e5ea0e4f80f00dc26be7d44cd86264029565b67
Alexey V. Butkeev <abutkeev@netup.ru>
2021-11-30 00:33:56 +0300

    (backend) add frontend stub

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT ?? 4000;
 
 const app = express();
+
+app.use(express.static(path.resolve('frontend')));
 
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`);
